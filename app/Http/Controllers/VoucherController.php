4aef1baca69fe61004a8b98b2781b5c39a4a1f3b
Roberto Arruda <roberto0arruda@hotmail.com>
2020-10-19 12:04:26 -0400

    extra: Permite cadastrar voucher individualmente para o cliente

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\VoucherResource;
 use App\Models\Customer;
+use App\Models\Offer;
 use App\Models\Voucher;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
@@ -18,14 +20,46 @@ class VoucherController extends Controller
      */
     public function index(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'email' => 'required|email:filter|exists:App\Models\Customer,email'
         ]);
 
-        $customerId = Customer::where('email', $request->input('email'))->first()->id;
+        $customerId = Customer::where('email', $validatedData['email'])->first()->id;
 
         $vouchers = Voucher::unused()->notExpired()->where('customer_id', $customerId)->with('offer')->get();
 
         return VoucherResource::collection($vouchers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|email:filter|exists:App\Models\Customer,email',
+            'offer_name' => 'required|exists:App\Models\Offer,name'
+        ]);
+
+        try {
+            $customerId = Customer::where('email', $validatedData['email'])->first()->id;
+            $offer = Offer::where('name', $validatedData['offer_name'])->first();
+
+            return Voucher::create([
+                'customer_id' => $customerId,
+                'offer_id' => $offer->id,
+                'expired_at' => $offer->expired_at
+            ])->refresh();
+
+        } catch (Exception $e) {
+            if (config('app.debug')) {
+                return ['message' => $e->getMessage()];
+            }
+
+            return ['message' => 'Erro ao criar Voucher individual'];
+        }
     }
 }
